2f2325632ecf88aa235985d8545dd934084f77af
Kevin Eduardo <kevimedu@gmail.com>
2015-10-25 01:56:07 -0200

    New language PT (brazillian)

---
+++ translate/lang-pt.php
@@ -0,0 +1,9 @@
+<?php  
+$tranEmail 		= "E-mail";
+$tranPass		= "Senha";
+$tranAccess		= "Acesso";
+$tranReg 		= "Registrar-se";
+$tranForget 	= "Esqueceu seus dados de login?";
+$tranSearch 	= "Centro de Busca";
+$descriptonSite = "Daigaku é uma ferramenta da web para instituições educacionais e seus estudantes.";
+?>
